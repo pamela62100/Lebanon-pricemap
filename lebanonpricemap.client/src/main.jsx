b5442a20684a2b6d5b1aa0e75aec337fadb0e3cb
pamela62100 <pamelasaliba62@gmail.com>
2026-03-06 23:06:39 +0200

    fixed rendering

--- lebanonpricemap.client/src/main.jsx
+++ lebanonpricemap.client/src/main.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import MapPage from './pages/MapPage';
-import './index.css';
-import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/map" element={<MapPage />} />
-            </Routes>
-        </BrowserRouter>
+        <App />
     </React.StrictMode>
 );
