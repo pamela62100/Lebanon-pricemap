613becdf37dfe4e7152a405e526f5069e64dd55c
pamela62100 <pamelasaliba62@gmail.com>
2026-03-12 00:46:00 +0200

    add database connection

--- LebanonPriceMap.Server/obj/Debug/net10.0/LebanonPriceMap.Server.AssemblyInfo.cs
+++ LebanonPriceMap.Server/obj/Debug/net10.0/LebanonPriceMap.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb09ac44b16784611f11a417261939cbbeb5815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47ecb85bf5e844a2529b5b4d5916a8de71e6abd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
