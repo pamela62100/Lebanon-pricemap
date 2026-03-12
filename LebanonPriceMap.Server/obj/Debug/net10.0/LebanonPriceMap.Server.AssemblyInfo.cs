b502746092e6807dc2482c10bea290b3cdacde76
Tarekbadawi12 <tarekbadawi123@outlook.com>
2026-03-12 17:07:06 +0200

    docs: Add rules part 2 detailing UI/UX features and store optimization logic.

--- LebanonPriceMap.Server/obj/Debug/net10.0/LebanonPriceMap.Server.AssemblyInfo.cs
+++ LebanonPriceMap.Server/obj/Debug/net10.0/LebanonPriceMap.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb09ac44b16784611f11a417261939cbbeb5815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8241941ca19321d678d94ac3f4020d1bc69a7742")]
 [assembly: System.Reflection.AssemblyProductAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
