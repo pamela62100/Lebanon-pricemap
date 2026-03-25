51cb999b2a5fe849c5b6119426a9c5de08e84ad1
pamela62100 <pamelasaliba62@gmail.com>
2026-03-25 17:05:08 +0200

    fix

--- LebanonPriceMap.Server/obj/Debug/net10.0/LebanonPriceMap.Server.AssemblyInfo.cs
+++ LebanonPriceMap.Server/obj/Debug/net10.0/LebanonPriceMap.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22eccd95f90789f4c3aed360a01317642f47c958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b568ee993a8030d5774fb9891527257c74546a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LebanonPriceMap.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
